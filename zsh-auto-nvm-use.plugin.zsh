eea8c6a596dc2efc65a994a608c7cd016e4ed23c
Nwel <nweldev@users.noreply.github.com>
2021-06-09 00:54:37 +0200

    fix: avoid changing too often
    don't run nvm use in directories where there is neither
    an nrvmrc nor a package.json file

--- zsh-auto-nvm-use.plugin.zsh
+++ zsh-auto-nvm-use.plugin.zsh
@@ -2,9 +2,9 @@ autoload -U add-zsh-hook
 load-nvmrc() {
   if [[ -f .nvmrc && -r .nvmrc ]]; then
     nvm use
-  elif [[ $(nvm version) != $(nvm version default)  ]]; then
-    echo "Reverting to nvm default version"
-    nvm use default
+  elif [[ $(nvm version) != $(nvm version default) && -f package.json && -r package.json ]]; then
+    echo "No nvmrc detected. You may want to revert to nvm default version."
+    #nvm use default
   fi
 }
 add-zsh-hook chpwd load-nvmrc
